a1edaf2859d116aed537f6a9f86b1ed790ed997d
Oreoyhp <2727035179@qq.com>
2019-12-31 00:24:41 +0800

    springboot结合微信公众号开发

---
+++ spring-boot-wechat/src/main/java/com/example/demo/entity/VideoMessage.java
@@ -0,0 +1,40 @@
+package com.example.demo.entity;
+
+import java.util.Map;
+
+public class VideoMessage extends BaseMessage{
+
+	/** 通过素材管理中的接口上传多媒体文件，得到的id */
+	private String mediaId;
+	/** 视频消息的标题 */
+	private String title;
+	/** 视频消息的描述 */
+	private String description;
+	
+	public String getMediaId() {
+		return mediaId;
+	}
+	public void setMediaId(String mediaId) {
+		this.mediaId = mediaId;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public VideoMessage(Map<String, String> requestMap, String mediaId, String title, String description) {
+		super(requestMap);
+		this.setMsgType("video");
+		this.mediaId = mediaId;
+		this.title = title;
+		this.description = description;
+	}
+
+}
